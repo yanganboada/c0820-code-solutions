d1d005162846e71a46a3a2ba31d5129dc92f5fd5
anniean8908 <anniean8908@gmail.com>
2020-10-07 21:56:26 +0000

    still working on seperator

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -3,15 +3,18 @@ function titleCase(string) {
   const lowerCase = ['a', 'an', 'and', 'as', 'at', 'but', 'by', 'en', 'for', 'if', 'in', 'nor', 'of', 'on', 'or', 'per', 'the', 'to'];
   const techWords = ['JavaScript', 'HTML', 'CSS', 'API'];
   const lowerTechWords = techWords.map(x => x.toLowerCase());
-  const seperator = [':'];
+  const seperator = [':', '-'];
   const word = string.split(' ');
 
   for (let i = 0; i < word.length; i++) {
-    // const lastWordEnd = word[i - 1].slice(-1);
-    // const checkSeperator = seperator.includes(lastWordEnd);
+    const lastWordEnd = word[i].slice(-1);
+    const checkSeperator = seperator.includes(lastWordEnd);
+    console.log(lastWordEnd, checkSeperator);
     if (lowerTechWords.includes(word[i])) {
       const index = lowerTechWords.indexOf(word[i]);
       word[i] = techWords[index];
+    } else if (checkSeperator) {
+      word[i + 1] = techWords[lowerTechWords.indexOf(word[i + 1])];
     } else if (!lowerCase.includes(word[i])) {
       word[i] = word[i].charAt(0).toUpperCase() + word[i].slice(1);
     }
@@ -19,4 +22,4 @@ function titleCase(string) {
   return word.join(' ');
 }
 
-// console.log(titleCase('speaking Javascript: an in-depth guide for programmers'));
+console.log(titleCase('speaking Javascript: an in-depth guide for programmers'));
