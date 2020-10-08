e3394fc97af212ad9aa896e121e614d678a5e3b8
anniean8908 <anniean8908@gmail.com>
2020-10-08 04:54:29 +0000

    String Manipulation 3 | Yang An Boada

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,25 +1,25 @@
 /* eslint-disable no-unused-vars */
 function titleCase(string) {
   const lowerCase = ['a', 'an', 'and', 'as', 'at', 'but', 'by', 'en', 'for', 'if', 'in', 'nor', 'of', 'on', 'or', 'per', 'the', 'to'];
-  const techWords = ['JavaScript', 'HTML', 'CSS', 'API'];
+  const techWords = ['JavaScript', 'HTML', 'CSS', 'API', 'JavaScript:'];
   const lowerTechWords = techWords.map(x => x.toLowerCase());
-  const seperator = [':', '-'];
   const word = string.split(' ');
+  let result = null;
 
   for (let i = 0; i < word.length; i++) {
-    const lastWordEnd = word[i].slice(-1);
-    const checkSeperator = seperator.includes(lastWordEnd);
-    console.log(lastWordEnd, checkSeperator);
+    word[i] = word[i].toLowerCase();
     if (lowerTechWords.includes(word[i])) {
       const index = lowerTechWords.indexOf(word[i]);
       word[i] = techWords[index];
-    } else if (checkSeperator) {
-      word[i + 1] = techWords[lowerTechWords.indexOf(word[i + 1])];
     } else if (!lowerCase.includes(word[i])) {
       word[i] = word[i].charAt(0).toUpperCase() + word[i].slice(1);
     }
+    result = word.join(' ');
   }
-  return word.join(' ');
-}
 
-console.log(titleCase('speaking Javascript: an in-depth guide for programmers'));
+  if (result.indexOf(':') > 0 || result.indexOf('-') > 0) {
+    result = result.replace(/an|the|depth/gi, x => x.charAt(0).toUpperCase() + x.slice(1));
+  }
+
+  return result;
+}
