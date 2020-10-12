36aa57942528ffb426678c272caa26fb9b409754
anniean8908 <anniean8908@gmail.com>
2020-10-12 05:25:26 +0000

    Working with Collection - Create Cards

--- working-with-collections/main.js
+++ working-with-collections/main.js
@@ -1,3 +1,24 @@
 /* global _ */
 /* eslint-disable no-console */
 console.log('Lodash is loaded:', typeof _ !== 'undefined');
+
+const cardsSuit = ['clubs', 'diamonds', 'hearts', 'spades'];
+const cardsRank = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
+const cardsData = [];
+
+function createCards(cardsSuit, cardsRank) {
+  cardsSuit.forEach(x => {
+    cardsRank.forEach(y => {
+      if (y === 'A') {
+        cardsData.push({ card: `${x} ${y}`, value: 11 });
+      } else if (y === 'J' || y === 'Q' || y === 'K') {
+        cardsData.push({ card: `${x} ${y}`, value: 10 });
+      } else {
+        cardsData.push({ card: `${x} ${y}`, value: `${y}` });
+      }
+    });
+  });
+  return cardsData;
+}
+
+console.log(createCards(cardsSuit, cardsRank));
