9c6028ddbb06736fba99a703f0fcd1cffb285c2c
anniean8908 <anniean8908@gmail.com>
2020-10-15 22:13:20 +0000

    Working with collections list score

--- working-with-collections/main.js
+++ working-with-collections/main.js
@@ -2,13 +2,15 @@
 /* eslint-disable no-console */
 console.log('Lodash is loaded:', typeof _ !== 'undefined');
 
+setTimeout(() => { console.log('Type "play()" in console for A New Game'); }, 1000);
+
 const cardsSuit = ['clubs', 'diamonds', 'hearts', 'spades'];
 const cardsRank = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
 const cardsData = [];
+const player = ['Annie', 'Tim D.', 'Cody', 'Uzair'];
+const cardsPerHand = 2;
 
-createCards(cardsSuit, cardsRank);
-
-function createCards(cardsSuit, cardsRank) {
+function createCards(suit, rank) {
   cardsSuit.forEach(x => {
     cardsRank.forEach(y => {
       if (y === 'A') {
@@ -16,22 +18,31 @@ function createCards(cardsSuit, cardsRank) {
       } else if (y === 'J' || y === 'Q' || y === 'K') {
         cardsData.push({ card: `${x} ${y}`, value: 10 });
       } else {
-        cardsData.push({ card: `${x} ${y}`, value: `${y}` });
+        cardsData.push({ card: `${x} ${y}`, value: y });
       }
     });
   });
-  shuffle(cardsData);
+  _.shuffle(cardsData);
+  return cardsData;
 }
 
-function shuffle(cardsData) {
-  let i = cardsData.length;
-  while (i !== 0) {
-    const randomIndex = Math.floor(Math.random() * i);
-    i--;
-    const holdVal = cardsData[i];
-    cardsData[i] = cardsData[randomIndex];
-    cardsData[randomIndex] = holdVal;
-  }
-  console.log(cardsData);
-  return cardsData;
+function play() {
+  createCards(cardsSuit, cardsRank);
+  const cardsSplitArray = _.chunk(cardsData, player.length * cardsPerHand);
+  cardsSplitArray.pop(-1);
+  let round = 1;
+  cardsSplitArray.forEach(x => {
+    if (round <= cardsSplitArray.length) {
+      console.log('-------------Round: ', round++, '--------------');
+      const eachPlayerArray = _.chunk(x, cardsPerHand);
+      for (let i = 0; i < player.length; i++) {
+        console.log(player[i], '\n',
+          'Card1: ', eachPlayerArray[i][0].card, '\n',
+          'Card2: ', eachPlayerArray[i][1].card, '\n',
+          'Total: ', eachPlayerArray[i][0].value + eachPlayerArray[i][1].value
+        );
+
+      }
+    }
+  });
 }
