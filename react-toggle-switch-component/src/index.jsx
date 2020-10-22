a1b0435309ebd073c64299131e5da98e15697d48
anniean8908 <anniean8908@gmail.com>
2020-10-22 18:52:16 +0000

    React Toggle Switch Component | Yang An Boada

---
+++ react-toggle-switch-component/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CreateSwitch from './toggle-switch';
+
+ReactDOM.render(
+  <CreateSwitch />,
+  document.getElementById('root')
+);
