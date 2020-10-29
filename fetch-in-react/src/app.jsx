ae2ebcf4f62ee8ea849c92930de3381981067427
anniean8908 <anniean8908@gmail.com>
2020-10-29 17:24:49 +0000

    Fetch in React -working on extra

--- fetch-in-react/src/app.jsx
+++ fetch-in-react/src/app.jsx
@@ -14,7 +14,7 @@ class App extends React.Component {
 
   componentDidMount() {
     /* your code here */
-    fetch('https://jsonplaceholder.typicode.com/uses')
+    fetch('https://jsonplaceholder.typicode.com/users')
       .then(res => {
         if (!res.ok) {
           this.setState({ error: `${res.status}` });
@@ -22,7 +22,7 @@ class App extends React.Component {
           res.json();
         }
       })
-      .then(res => res.json())
+      // .then(res => res.json())
       .then(data => this.setState({ users: data, isLoading: false }))
       .catch(err => this.setState({ error: err.message }));
   }
@@ -30,7 +30,7 @@ class App extends React.Component {
   render() {
     return this.state.isLoading
       ? this.state.error
-        ? <p>{this.state.error}</p>
+        ? consol <p>{this.state.error}</p>
         : <p>Loading...</p>
       : <UserList users={this.state.users}/>;
   }
