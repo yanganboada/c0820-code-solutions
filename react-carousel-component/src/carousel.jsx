98af0466e972373a6b404b03dbcbb9335aeaa5b1
anniean8908 <anniean8908@gmail.com>
2020-11-19 08:33:32 +0000

    React Carousel Component | Yang An Boada

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -5,40 +5,59 @@ class Carousel extends React.Component {
     super(props);
     this.props = props;
     this.state = {
-      isContolClicked: false
+      index: 0
     };
     this.handlePreviousClick = this.handlePreviousClick.bind(this);
     this.hangleNextClick = this.hangleNextClick.bind(this);
   }
 
-  handlePreviousClick(e) {
-    console.log(e);
-  }
-
-  hangleNextClick(e) {
-    console.log(e);
-  }
-
-  handleDotCLick(e) {
-    console.log(e);
+  componentDidMount() {
+    this.autoPlay();
   }
 
   autoPlay() {
+    const interval = setInterval(() => {
+      const index = this.state.index !== this.props.list.length - 1
+        ? this.state.index + 1
+        : 0;
+      this.setState({ index });
+    }, 3000);
+    this.setState({ interval });
+  }
 
+  handlePreviousClick() {
+    const index = this.state.index !== 0 ? this.state.index - 1 : this.props.list.length - 1;
+    this.setState({ index });
+  }
+
+  hangleNextClick() {
+    const index = this.state.index !== this.props.list.length - 1 ? this.state.index + 1 : 0;
+    this.setState({ index });
   }
 
   render() {
     return (
       <div className="carousel">
         <div className="carousel-body">
-          <i className="fas fa-chevron-left fa-3x" onClick={this.handlePreviousClick}></i>
-          {this.props.list.map((poke, index) => <img src={poke.imageUrl} key={index} alt={poke.name} />)}
-          <i className="fas fa-chevron-right fa-3x" onClick={this.handleNextClick}></i>
+          <i className="fas fa-chevron-left fa-3x cursor"
+            onClick={this.handlePreviousClick}>
+          </i>
+          {this.props.list.map((poke, index) => {
+            return <img
+              className={this.state.index !== index ? 'hide' : ''}
+              src={poke.imageUrl}
+              key={index}
+              alt={poke.name} />;
+          })}
+          <i className="fas fa-chevron-right fa-3x cursor"
+            onClick={this.hangleNextClick}>
+          </i>
         </div>
         <div className="dots">
-          {this.props.list.forEach(index => <i className="fas fa-circle" key={index} onClick={this.handleDotCLick}></i>)}
-          <i className="fas fa-circle"></i>
-          <i className="far fa-circle"></i>
+          {this.props.list.map((poke, index) => (
+            <i className={`${index === this.state.index ? 'fas' : 'far'} fa-circle`}
+              onClick={() => this.setState({ index })}
+              key={index}></i>))}
         </div>
       </div>
     );
